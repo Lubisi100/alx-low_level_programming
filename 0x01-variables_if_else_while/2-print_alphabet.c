290b263ec8b2ec9994cab68fc0baa9308177d025
Lubisi100 <vinahlubisi@gmail.com>
2023-09-11 07:50:08 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
