4f6898e6bfc81bc01564bf3a5ed37a9bf74653f0
Lubisi100 <vinahlubisi@gmail.com>
2023-09-11 02:09:01 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stio.h>
+#include <stdio.h>
 
 /**
  * main - This is the main function
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
