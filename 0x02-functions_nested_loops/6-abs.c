d9b809e51fcfde0bc1c9ae4708bf7f56a2a72c90
Lubisi100 <vinahlubisi@gmail.com>
2023-09-13 09:11:32 -0700

    no such thing as an absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function computes the absolute positive value of an integer
+ * @v: parameter to be checked
+ * Return: -v or v
+ */
+int _abs(int v)
+{
+	if (v < 0)
+		return (-v);
+	else
+		return (v);
+
+}
