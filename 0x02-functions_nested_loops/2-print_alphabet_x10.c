49080151159641000b739e8c1e237366ac9daf72
Lubisi100 <vinahlubisi@gmail.com>
2023-09-13 08:13:14 -0700

    10times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints x10 times the alphabet
+ *
+ * Return: 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char v;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (v = 'a' ; v <= 'z' ; v++)
+			_putchar(v);
+	}
+	_putchar('\n');
+}
